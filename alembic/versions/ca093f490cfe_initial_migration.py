ceb62102380d1cf37c0d7b42547f0d1da219e66d
sirko2a1 <134325906+sirko2a1@users.noreply.github.com>
2023-11-08 20:11:14 +0200

    commit №2

--- alembic/versions/ca093f490cfe_initial_migration.py
+++ alembic/versions/ca093f490cfe_initial_migration.py
@@ -21,6 +21,48 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     pass
 
+from alembic import op
+import sqlalchemy as sa
 
-def downgrade() -> None:
-    pass
+def upgrade():
+    op.create_table(
+        'students',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String),
+        sa.Column('group_id', sa.Integer, sa.ForeignKey('groups.id'))
+    )
+
+    op.create_table(
+        'groups',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String)
+    )
+
+    op.create_table(
+        'teachers',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String)
+    )
+
+    op.create_table(
+        'subjects',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String),
+        sa.Column('teacher_id', sa.Integer, sa.ForeignKey('teachers.id'))
+    )
+
+    op.create_table(
+        'grades',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('student_id', sa.Integer, sa.ForeignKey('students.id')),
+        sa.Column('subject_id', sa.Integer, sa.ForeignKey('subjects.id')),
+        sa.Column('grade', sa.Integer),
+        sa.Column('date', sa.Date)
+    )
+
+def downgrade():
+    op.drop_table('grades')
+    op.drop_table('subjects')
+    op.drop_table('teachers')
+    op.drop_table('students')
+    op.drop_table('groups')
